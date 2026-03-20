891e1bef4c1e9197f34526cba704783c8f4ca444
Matheus Ribeiro <mathribemathribe@gmail.com>
2026-03-20 12:45:03 -0300

    feat: Model da classe usuario e criaçao das funçoes de criar hash_senha e verificar senha

--- app/models/tarefas.py
+++ app/models/tarefas.py
@@ -2,6 +2,7 @@ from app.database import db
 from datetime import datetime
 
 class Tarefa(db.Model):
+    __tablename__ = "tarefas"
     id = db.Column(db.Integer,primary_key=True)
     titulo = db.Column(db.String(100),nullable = False)
     descricao = db.Column (db.String(100),nullable = True)
