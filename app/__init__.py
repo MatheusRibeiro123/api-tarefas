a383b84183096a5b1cf0c43ce4c3ad28a4177457
Matheus Ribeiro <mathribemathribe@gmail.com>
2026-03-20 20:11:35 -0300

    feat: criaçao das rotas de criaçao de usuario, login e blueprint auth

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from app.database import db
-
+from app.routes.usuario import auth_bp
 def create_app():
     app = Flask(__name__)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
     db.init_app(app)
-    
+   
+    app.register_blueprint(auth_bp)
+   
     @app.route("/")
     def statusAPI():
         return "API Funcionando"
---
+++ app/routes/usuario.py
@@ -0,0 +1,64 @@
+from app.database import db
+from flask import Blueprint,request,jsonify
+from app.models.usuario import Usuario
+auth_bp = Blueprint("auth",__name__, url_prefix="/auth")
+
+
+#Rota criar usuario
+@auth_bp.route("/register", methods = ["POST"])
+def register():
+    dados = request.json
+    
+    if not dados:
+        return jsonify({"erro":"Dados não enviados"}),400
+    
+    nome = dados.get("nome")
+    email = dados.get("email")
+    senha = dados.get("senha")
+
+    if not nome or not email or not senha:
+        return jsonify({"erro":"Nome, email e senha são obrigatorios!"}),400
+    
+    usuario_existente= Usuario.query.filter_by(email = email).first()
+
+    if usuario_existente:
+
+        return jsonify({"erro":"Usuario ja cadastrado"}),409
+    
+    usuario = Usuario(
+        nome = nome,
+        email =email
+    )
+    usuario.definir_senha(senha)
+
+    db.session.add(usuario)
+    db.session.commit()
+
+    return jsonify({"sucesso":"Usuario criado com sucesso"}),201
+
+#rota fazer login
+
+@auth_bp.route("/login",methods = ["POST"])
+def login():
+
+    dados = request.json
+ 
+    if not dados:
+        return jsonify({"erro":"dados não enviados"}),400
+    
+    email = dados.get("email")
+    senha = dados.get("senha")
+
+    if not email or not senha:
+        return jsonify({"erro":"email e senha são obrigatorios!"}),400
+    
+    usuario = Usuario.query.filter_by(email = email).first()
+
+    if usuario and usuario.verificar_senha(senha):
+        return jsonify({"sucesso":"Login realizado com sucesso"}) ,200
+    
+    return jsonify({"erro":"Usuario ou senha invalidos"}), 401
+        
+
+    
+
